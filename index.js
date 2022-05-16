11c9e6ed9b94145313d88f46139b7c941804a921
brody.sanford@gmail.com <brodysmac@Brodens-MBP.fios-router.home>
2022-05-16 15:34:44 -0400

    commit tree 5df0533eea4cfebf0795a70a4d18e50e0c8a3098

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(startBlock) {
+    return Math.abs(startBlock - 42) 
+}
+
+function distanceFromHqInFeet(startBlock) {
+    return distanceFromHqInBlocks(startBlock) * 264
+}
+
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs((endBlock - startBlock) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) - 400 <= 0) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) - 400 <= 1600) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    } 
+    else if (distanceTravelledInFeet(start, destination) - 400 < 2100) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) -400 >= 2100) {
+        return 'cannot travel that far'
+    }
+}
